d7afe99fd0f24527edcb833bde1fd56df81cc43b
TeresaPhuong <tranphuongnd91@gmail.com>
2016-09-23 14:19:09 +0700

    Add testcase.

---
+++ LogigearQATeam/TestSuites/ArticleFeature/ArticleFeature.feature
@@ -0,0 +1,14 @@
+﻿Feature: ArticleFeature
+	
+Scenario: User can add new article
+	Given I naviate to Joomla site: http://192.168.189.119/abyssal/administrator/
+	And I enter username: phuong.thi.tran
+	And I enter password: 123456
+	And I click Login button
+	When I click New Article button
+	And I enter title: Using Branches into title textbox
+	And I enter content: This tutorial is a comprehensive introduction to Git branches. into content textbox
+	And I select value: Images of Category list
+	And I click Save and Close button
+	Then The successful message displays
+	And The new article displays in Article Manage table
---
+++ LogigearQATeam/TestSuites/ArticleFeature/TestCase1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace LogigearQATeam.TestSuites.ArticleFeature
+{
+    [Binding]
+    public sealed class TestCase1
+    {
+        [Given(@"I naviate to Joomla site: (.*)")]
+        public void GivenINaviateToJoomlaSite(string URL)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I enter username: (.*)")]
+        public void GivenIEnterUsernamePhuong_Thi_Tran(string username)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I enter password: (.*)")]
+        public void GivenIEnterPassword(int password)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I click Login button")]
+        public void GivenIClickLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click New Article button")]
+        public void WhenIClickNewArticleButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter title: (.*)")]
+        public void WhenIEnterTitleUsingBranchesIntoTitleTextbox(string title)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter content: (.*)")]
+        public void WhenIEnterContentThisTutorialIsAComprehensiveIntroductionToGitBranches_IntoContentTextbox(string content)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I select value: (.*) of Category list")]
+        public void WhenISelectValueImagesOfCategoryList(string category)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click Save and Close button")]
+        public void WhenIClickSaveAndCloseButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"The successful message displays")]
+        public void ThenTheSuccessfulMessageDisplays()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"The new article displays in Article Manage table")]
+        public void ThenTheNewArticleDisplaysInArticleManageTable()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
