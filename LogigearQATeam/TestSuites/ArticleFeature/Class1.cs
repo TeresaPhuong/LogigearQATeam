d301a84c886331330022a7807c435221b3c85f09
TeresaPhuong <tranphuongnd91@gmail.com>
2016-09-23 10:39:07 +0700

    Phuong commit

---
+++ LogigearQATeam/TestSuites/ArticleFeature/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogigearQATeam.TestSuites.ArticleFeature
+{
+    class Class1
+    {
+        public void demo()
+        {
+            string a = "abc";
+        }
+    }
+}
